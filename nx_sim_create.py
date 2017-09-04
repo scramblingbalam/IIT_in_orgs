8db14fe8235e13daf5d7f3483a27f53aa9770c2c
Colin A. Drayton <cdrayton@umich.edu>
2017-09-04 14:26:01 -0400

    ADDED function to add random weightings, form 0 to 1 to the edges this is the closes #3

--- nx_sim_create.py
+++ nx_sim_create.py
@@ -7,19 +7,22 @@ Created on Sun Aug 13 16:35:16 2017
 import itertools as it
 import networkx as nx
 import random
-n = 100 # number of nodes
-m = 1000 # number of edges
+import time
+import numpy as np
+n = 10 # number of nodes
+m = 50 # number of edges
 p = 0.3#probability of triad closer
-m = int(float(m)/float(n))+2
+r = 0.3
+m = int(float(m)/float(n))+1
 print(m)
 G = nx.powerlaw_cluster_graph(n, m, p)
 #print(nx.triangles(G))
 #print(nx.clustering(G))
-print(nx.average_clustering(G))
-print(len(G.edges()))
+print("Average Clustering",nx.average_clustering(G))
+print("edge_num",G.size())
 GD = G.to_directed()
-print(len(GD.edges()))
-print(GD.nodes())
+print("edge_num",GD.size())
+print(GD.nodes(),"Node list")
 
     
 def reciprocity_ratio(gd):
@@ -30,10 +33,11 @@ def reciprocity_ratio(gd):
             reciprocal += 1.0
     return reciprocal/gd.size()
 
-print(reciprocity_ratio(GD))
+print("reciprocity ratio",reciprocity_ratio(GD))
 
 def change_reciprocity_ratio(gd,r):
     if r >= 0.0 and r <= 1.0:
+        r = r *(-1)
         del_times = int(r*gd.size())+1
         print(del_times)
         for i in range(del_times):
@@ -42,8 +46,33 @@ def change_reciprocity_ratio(gd,r):
 #        return gd
     else:
         raise ValueError("value of 'r' must be between 1 and 0")
-    
-change_reciprocity_ratio(GD,0.5)
-print(reciprocity_ratio(GD))
 
-print(GD.size())
+def random_weights(Graph,percision=3):
+    """
+    Adds random weights between 1 and 0 with a given percision
+    """
+    percision = percision + 2
+    for i in Graph.edges_iter():
+        ran_str = str(random.random())
+        if len(ran_str) > percision:
+            ran = float(ran_str[:percision])
+        else:
+            ran = float(ran_str)
+        Graph[i[0]][i[1]]['weight'] = ran
+
+
+
+change_reciprocity_ratio(GD,r)
+random_weights(GD)
+print("edge_num",GD.size())
+print("Node Num", GD.order())
+print("reciprocity ratio",reciprocity_ratio(GD))
+print("Average Clustering",nx.average_clustering(GD))
+path_start = time.time()
+
+paths = nx.all_simple_paths(GD,0,1)
+path_end = time.time()
+print(len(list(paths)))
+print("ALL path time",path_end-path_start)
+
+
