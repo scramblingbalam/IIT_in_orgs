c50f0dfa7dbf9e68f225e86a95d823eb7257ff97
Colin A. Drayton <cdrayton@umich.edu>
2017-09-04 14:30:30 -0400

    ADDED function that calculates the conductnace of between two node using the NetworkX all_simple_paths function, this built in function throws a memory error on my 16GB windows 10 machine at node = 100 edge = 875 scales per #2, this is the closes #4

--- nx_sim_create.py
+++ nx_sim_create.py
@@ -47,7 +47,7 @@ def change_reciprocity_ratio(gd,r):
     else:
         raise ValueError("value of 'r' must be between 1 and 0")
 
-def random_weights(Graph,percision=3):
+def radom_weights(Graph,percision=3):
     """
     Adds random weights between 1 and 0 with a given percision
     """
@@ -60,14 +60,42 @@ def random_weights(Graph,percision=3):
             ran = float(ran_str)
         Graph[i[0]][i[1]]['weight'] = ran
 
+def pairwise(iterable):
+    "s -> (s0,s1), (s1,s2), (s2, s3), ..."
+    a, b = it.tee(iterable)
+    next(b, None)
+    return zip(a, b)
+
+def conductance(g,i,j):
+    """ calculates the conductance between two nodes in a graph
+        ARGS:
+            g a graph
+            i index of starting node
+            j index of ending node
+        returns:
+            float: conductance mesasure
+    """
+    C = []
+    paths = nx.all_simple_paths(g,0,1)
+#    print(len(list(paths)))
+#    print(len(list(paths)),"LENGHT")
+    for path in list(paths):
+        path_calc = []
+        for k,l in pairwise(path):
+            Wkl = g[k][l]['weight']
+            Dk = g.out_degree(k)
+            path_calc.append(Wkl/Dk)
+        C.append(np.prod(path_calc))
+    return sum(C)
+
+
 
 
 change_reciprocity_ratio(GD,r)
-random_weights(GD)
+radom_weights(GD)
 print("edge_num",GD.size())
 print("Node Num", GD.order())
 print("reciprocity ratio",reciprocity_ratio(GD))
-print("Average Clustering",nx.average_clustering(GD))
 path_start = time.time()
 
 paths = nx.all_simple_paths(GD,0,1)
@@ -75,4 +103,8 @@ path_end = time.time()
 print(len(list(paths)))
 print("ALL path time",path_end-path_start)
 
-
+con_start = time.time()
+con_metric = conductance(GD,0,1)
+con_end = time.time()
+print(con_metric)
+print("Conductance time",con_end-con_start)
